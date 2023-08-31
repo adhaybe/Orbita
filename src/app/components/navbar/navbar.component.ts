87b2394bd9f523118a6a0180694e8e612e7b5e31
adhaybe <ad.haybe1@gnail.com>
2023-08-31 23:45:08 +0100

    Created nav bar and made it responsive

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  planets = [
+    { name: 'Mercury', route: '/mercury', icon: 'mercury' },
+    { name: 'Venus', route: '/venus', icon: 'venus' },
+    { name: 'Earth', route: '/earth', icon: 'earth' },
+    { name: 'Mars', route: '/mars', icon: 'mars' },
+    { name: 'Jupiter', route: '/jupiter', icon: 'jupiter' },
+    { name: 'Saturn', route: '/saturn', icon: 'saturn' },
+    { name: 'Uranus', route: '/uranus', icon: 'uranus' },
+    { name: 'Neptune', route: '/neptune', icon: 'neptune' },
+  ];
+
+  isMobileView: boolean = false;
+  isOverlayVisible: boolean = false;
+
+  ngOnInit(): void {
+    this.setMobileView();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(_event: any): void {
+    this.setMobileView();
+  }
+
+  toggleOverlay() {
+    this.isOverlayVisible  = !this.isOverlayVisible;
+  }
+
+  private setMobileView(): void {
+    this.isMobileView = window.innerWidth <= 666;
+  }
+}
